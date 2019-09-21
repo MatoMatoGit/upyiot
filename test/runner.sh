0ca0584be53514f011262feaf13d0cbc2a4736c4
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2019-09-21 13:14:07 +0200

    + test/runner: Optional argument to run specfiic test.

--- test/runner.sh
+++ test/runner.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+TEST=${1:-all}
+
+echo "$TEST"
+
 FILES="./test_*.py"
+
+if [ $TEST = all ]; then
+echo "Running all tests ..." 
 for f in $FILES
 do
-  echo "Running test: $f ..."
+  echo "Running test: ${f%.*} ..."
 
   ../build/micropython ./main.py ${f%.*}
 done
+else
+echo "Running test: $TEST ..."
+../build/micropython ./main.py ${TEST%.*}
+fi
+
