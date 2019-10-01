2eb71afcd9992d655be3777c3f398181994e4d69
Dorus <spijker.dorus@gmail.com>
2019-10-01 16:32:33 +0200

    * src/module/Power/Power: Moved Service into PowerManager class. * src/module/Power/Power: Refactoring after tests.

--- src/module/Power/Power.py
+++ src/module/Power/Power.py
@@ -8,9 +8,10 @@ except:
     
 import uerrno as errno
 from micropython import const
-from drivers import Battery
+from drivers.Battery import Battery
 from middleware.StructFile import StructFile
 from middleware.SubjectObserver.SubjectObserver import Subject
+from middleware.Singleton import Singleton
 
 SLEEP_TIME_MAX_SEC = const(86400)
 
@@ -19,9 +20,9 @@ class PowerSupply(object):
     EN_POLARITY_ACTIVE_LOW   = const(0)
     EN_POLARITY_ACTIVE_HIGH  = const(1)
     
-    def __init__(self, en_pin, en_polarity, voltage):
+    def __init__(self, en_pin_nr, en_polarity, voltage):
         self.Voltage = voltage
-        self.PinEnable = Pin(en_pin, Pin.OUT)
+        self.PinEnable = Pin(en_pin_nr, Pin.OUT)
         self.Enabled = False
         self.Polarity = en_polarity
         self.Enable(False)
@@ -43,7 +44,7 @@ class ServicePowerManager(object):
         self.Supply = supply_obj
         self.Voltage = voltage
         self.SleepTime = sleep_time_sec
-        self.RemaningTime = self.SleepTime
+        self.RemainingTime = self.SleepTime
         self.Asleep = False
            
     def Suspend(self):
@@ -52,42 +53,39 @@ class ServicePowerManager(object):
         
         self.Asleep = True
 
-    def _RemainingTimeSet(self, remaining_time_sec):
-        self.RemaningTime = remaining_time_sec
+    def RemainingTimeSet(self, remaining_time_sec):
+        self.RemainingTime = remaining_time_sec
         
     def _AsleepReset(self):
         self.Asleep = False
 
 
-class PowerManager(object):
+class PowerManager:
     
     SERVICE_INTERVAL_SEC    = const(10)
     BATTERY_LOW_SLEEP_SEC   = const(3600)
     SLEEP_DATA_FMT          = "<II"
     SLEEP_FILE_NAME         = "/sleep"
-    
-    _ServiceInit = False
-    
+
     def __init__(self, directory, battery, battery_level_threshold, min_sleep_duration_sec):
-        
+
         # Create a set for all BeforeSleep callbacks.
         self.CbsBeforeSleep = set()
         # Create a set for all LowBattery callbacks.
         self.CbsLowBattery = set()
-        # Create a set for all ServicePowerManager objects.
-        self.ServicePowerManagers = set()
+        # Create a lsit for all ServicePowerManager objects.
+        self.ServicePowerManagers = []
         # Create a set for all PowerSupply objects.
         self.PowerSupplies = set()
-        
+
         self.Battery = battery
         self.BatteryLevel = Subject()
         self.BatteryLevelThreshold = battery_level_threshold
         self.MinSleepDuration = min_sleep_duration_sec
-        self.SleepFile = StructFile.StructFile(directory + PowerManager.SLEEP_FILE_NAME, 
+        self.SleepData = StructFile.StructFile(directory + PowerManager.SLEEP_FILE_NAME,
                                                PowerManager.SLEEP_DATA_FMT)
-        # Load previous sleep time
-        if self.SleepFile.Count > 0:
-            self.SleepDuration = StructFile.ReadData(self.SleepFile.Count)[0]
+
+        self.ServiceInit = False
 
     def WokeFromSleep(self):
         # Check the reset cause to determine if the device was
@@ -104,13 +102,14 @@ class PowerManager(object):
         # Check if the service power manager has a required voltage which has not been
         # linked to a supply yet.
         if svc_power_mngr_obj.Supply is None and svc_power_mngr_obj.Voltage is not None:
+            print(svc_power_mngr_obj.Voltage)
             # Get a supply from the required voltage.
-            self._SupplyFromVoltageReq(svc_power_mngr_obj.Voltage)
+            svc_power_mngr_obj.Supply = self._SupplyFromVoltageReq(svc_power_mngr_obj.Voltage)
             # No supply found.
             if svc_power_mngr_obj.Supply is None:
                 return errno.EEXIST
         # Add the service power manager.
-        self.ServicePowerManagers.add(svc_power_mngr_obj)
+        self.ServicePowerManagers.append(svc_power_mngr_obj)
         return 0
         
     def RegisterPowerSupply(self, power_supply_obj):
@@ -125,11 +124,12 @@ class PowerManager(object):
             
     def _NotifyLowBattery(self):
         for cb in self.CbsLowBattery:
+            print(cb)
             cb()
    
     def _Sleep(self, seconds):
         # Notify all registered callbacks
-        PowerManager._NotifyBeforeSleep()
+        self._NotifyBeforeSleep()
         machine.deepsleep(seconds)   
                  
     def _SleepForever(self):
@@ -138,80 +138,98 @@ class PowerManager(object):
         machine.deepsleep()
         
     def _SupplyFromVoltageReq(self, voltage):
+        print("Finding supply for voltage req: {}".format(voltage))
         for supply in self.PowerSupplies:
-            if supply.Voltage is voltage:
-                return PowerSupply
-        
+            print(supply.Voltage)
+            if supply.Voltage == voltage:
+                print("Found suitable supply")
+                return supply
+        print("No supply found")
         return None
-                
 
-def Service():
-    if PowerManager._ServiceInit is False:
-        rem_time = 0
-        i = 0
-        # Loop through the sleep data in the sleep file
-        # and calculate (and set) the remaining time.
-        for sleep_data in PowerManager.SleepFile:
-            try:
-                rem_time = sleep_data[0] - PowerManager.SleepDuration
+    def Service(self):
+        if self.ServiceInit is False:
+            print("Initializing Service")
+
+            print("Entries in sleep file: {}".format(self.SleepData.Count))
+            # Load previous sleep time
+            if self.SleepData.Count > 0:
+                sleep_duration = self.SleepData.ReadData(self.SleepData.Count - 1)[0]
+                print("Woke up from %d second sleep." % sleep_duration)
+
+            # Loop through the sleep data in the sleep file
+            # and calculate (and set) the remaining time.
+            for i in range(0, self.SleepData.Count - 1):
+                print("Reading sleep file entry: {}".format(i))
+                sleep_data = self.SleepData.ReadData(i)
+                rem_time = sleep_data[0] - sleep_duration
                 if rem_time <= 0:
-                    rem_time = PowerManager.ServicePowerManagers[i].SleepTime
-                PowerManager.ServicePowerManagers[i]._RemainingTimeSet(rem_time)
-            except:
-                print("Error setting remaining sleep time.")
-            i = i + 1
-        PowerManager._ServiceInit = True
-    
-    # Read the battery level.
-    PowerManager.BatteryLevel = PowerManager.Battery.ReadLevel()
-    
-    # Check if the current battery level is below the threshold.
-    # If this is the case the device will go to sleep even when not 
-    # requested.
-    if PowerManager.BatterLevel < PowerManager.BatteryLevelThreshold:
-        PowerManager._NotifyLowBattery()
-        PowerManager._Sleep(PowerManager.BATTERY_LOW_SLEEP_SEC * 1000)
-    
-    # Clear the sleep file to store new sleep data.
-    PowerManager.SleepFile.Clear()
-    
-    # Set variables used in the scheduling loop.
-    goto_sleep = True
-    PowerManager.SleepDuration = SLEEP_TIME_MAX_SEC
-    rem_time = 0
-    
-    # Loop through the set of coro power managers and check if each of them
-    # is ready to sleep. 
-    for sched in PowerManager.ServicePowerManagers:
-        if sched.Asleep is True:
-            
-            # Get the remaining sleep time for the manager and append it
-            # to the sleep data file.
-            rem_time = sched.RemainingTime
-            PowerManager.SleepFile.AppendData(rem_time)
-            
-            # Check if the remaining time is greater than the minimal sleep
-            # time.
-            if rem_time >= PowerManager.MinSleepTime:
-                # Save the remaining time for this manager if it is the smallest amount
-                # of time of all schedulers.                       
-                if rem_time < PowerManager.SleepDuration:
-                    PowerManager.SleepDuration = rem_time
-            
-            # If the task has a small amount of sleep time left to sleep, let the
-            # task run until the next cycle when a larger amount is remaining.
-            else:
-                sched.AsleepReset()
+                    rem_time = self.ServicePowerManagers[i].SleepTime
+                print("Remaining time: {}".format(rem_time))
+                self.ServicePowerManagers[i].RemainingTimeSet(rem_time)
+            self.ServiceInit = True
+            print("Initialized")
+
+        # Read the battery level.
+        self.BatteryLevel.State = self.Battery.LevelRead()
+        # Check if the current battery level is below the threshold.
+        # If this is the case the device will go to sleep even when not
+        # requested.
+        if self.BatteryLevel.State <= self.BatteryLevelThreshold:
+            print("Battery level below threshold")
+            self._NotifyLowBattery()
+            self._Sleep(PowerManager.BATTERY_LOW_SLEEP_SEC * 1000)
+
+        if self.SleepData.Count > 0:
+            print("Clearing sleep data file")
+            # Clear the sleep file to store new sleep data.
+            self.SleepData.Clear()
+
+        # Set variables used in the scheduling loop.
+        if len(self.ServicePowerManagers) > 0:
+            goto_sleep = True
         else:
             goto_sleep = False
-            break
-    
-    # If all coros indicate to go to sleep.
-    if goto_sleep is True:
-        # Append the actual sleep duration as last entry and
-        # go to sleep.
-        PowerManager.SleepFile.AppendData(PowerManager.SleepDuration)
-        PowerManager._Sleep(PowerManager.SleepDuration)
+        sleep_duration = SLEEP_TIME_MAX_SEC
 
-    
-        
+        # Loop through the set of coro power managers and check if each of them
+        # is ready to sleep.
+        for svc_pwr_mngr in self.ServicePowerManagers:
+            if svc_pwr_mngr.Asleep is True:
+                print("{} is alseep".format(svc_pwr_mngr))
+                # Get the remaining sleep time for the manager and append it
+                # to the sleep data file.
+                rem_time = svc_pwr_mngr.RemainingTime
+                self.SleepData.AppendData(rem_time, 0)
+                print("Remaining time: {} sec".format(rem_time))
+
+                # Check if the remaining time is greater than the minimal sleep
+                # time.
+                if rem_time >= self.MinSleepDuration:
+                    print("Remaining time qualifies for a sleep request")
+                    # Save the remaining time for this manager if it is the smallest amount
+                    # of time of all ServicePowerManagers.
+                    if rem_time < sleep_duration:
+                        sleep_duration = rem_time
+                        print("New sleep duration: {} sec".format(sleep_duration))
+
+                # If the task has a small amount of sleep time left to sleep, let the
+                # task run until the next cycle when a larger amount is remaining.
+                else:
+                    print("Resetting sleep state")
+                    svc_pwr_mngr.AsleepReset()
+                    goto_sleep = False
+                    break
+            else:
+                print("{} is NOT alseep".format(svc_pwr_mngr))
+                goto_sleep = False
+                break
+
+        # If all coros indicate to go to sleep.
+        if goto_sleep is True:
+            # Append the actual sleep duration as last entry and
+            # go to sleep.
+            self.SleepData.AppendData(sleep_duration, 0)
+            self._Sleep(sleep_duration)
+
+        print("Exiting Service")
