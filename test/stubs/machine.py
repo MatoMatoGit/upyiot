6e1d20a5b2d75ce7467702510f32280abdf63d13
Dorus <spijker.dorus@gmail.com>
2019-10-01 16:30:54 +0200

    + test/stubs/machine: Functions is_asleep, wake_up and asleep_for.

--- test/stubs/machine.py
+++ test/stubs/machine.py
@@ -2,8 +2,39 @@ from micropython import const
 
 DEEPSLEEP_RESET = const(0)
 
+Asleep = False
+SleepDuration = 0
+
 def reset_cause():
     return DEEPSLEEP_RESET
 
+
 def deepsleep(t=None):
-    return
+    global Asleep
+    global SleepDuration
+
+    print("Going to sleep")
+    if t is not None:
+        print("for %d seconds" % t)
+    Asleep = True
+    SleepDuration = t
+
+
+def is_asleep():
+    global Asleep
+
+    return Asleep
+
+
+def wake_up():
+    global Asleep
+    global SleepDuration
+
+    Asleep = False
+    SleepDuration = 0
+
+
+def asleep_for():
+    global SleepDuration
+
+    return SleepDuration
