d012f63463fd7f6ca4ae620f2dbf05c726d7928a
Dorus <spijker.dorus@gmail.com>
2019-10-08 20:46:17 +0200

    + src/middleware/NvQueue/NvQueue: Non-Volatile Queue, FIFO queue of structs in a file. + test/test_NvQueue.

---
+++ src/middleware/NvQueue/NvQueue.py
@@ -0,0 +1,103 @@
+from middleware.StructFile import StructFile
+from micropython import const
+
+
+class NvQueueIterator(object):
+
+    def __init__(self, nvqueue_obj):
+        print(nvqueue_obj)
+        nvqueue_obj.Queue.IteratorConfig(nvqueue_obj.ReadOffset,
+                                              nvqueue_obj.WriteOffset, nvqueue_obj.Count)
+        self.Iterator = iter(nvqueue_obj.Queue)
+
+    def __next__(self):
+        return self.Iterator.__next__()
+
+
+class NvQueue:
+
+    # Capacity, read offset, write offset.
+    META_FMT = "<HHHH"
+    META_STRUCT_CAP     = const(0)
+    META_STRUCT_CNT     = const(1)
+    META_STRUCT_R_OFF   = const(2)
+    META_STRUCT_W_OFF   = const(3)
+
+    def __init__(self, file, data_fmt, capacity):
+        self.Queue = StructFile.StructFile(file, data_fmt, NvQueue.META_FMT)
+        self.QueueIterator = None
+
+        meta = self.Queue.ReadMeta()
+        print(meta)
+        # If no metadata is present in the queue file, reset the metadata and write
+        # it to the file.
+        if meta is None:
+            print("No metadata present, initializing queue.")
+            self.Capacity = capacity
+            self._MetaReset()
+            self._MetaUpdate()
+        else:
+            print("Existing metadata found.")
+            self.Capacity = meta[NvQueue.META_STRUCT_CAP]
+            self.Count = meta[NvQueue.META_STRUCT_CNT]
+            self.ReadOffset = meta[NvQueue.META_STRUCT_R_OFF]
+            self.WriteOffset = meta[NvQueue.META_STRUCT_W_OFF]
+
+            # If the read capacity differs from the capacity argument, update it.
+            if self.Capacity is not capacity:
+                print("Capacity changed, updating metadata.")
+                self.Capacity = capacity
+                self._MetaUpdate()
+
+    def __iter__(self):
+        print("Creating iter")
+        return NvQueueIterator(self)
+
+    def Push(self, *item):
+        if self.Space() is 0:
+            return -1
+
+        self.Queue.WriteData(self.WriteOffset, *item)
+        self.WriteOffset = self._OffsetAdvance(self.WriteOffset)
+        self._CountInc()
+        self._MetaUpdate()
+        return self.Count
+
+    def Pop(self):
+        if self.Count is 0:
+            return None
+
+        data = self.Queue.ReadData(self.ReadOffset)
+        self.ReadOffset = self._OffsetAdvance(self.ReadOffset)
+        self._CountDec()
+        self._MetaUpdate()
+        return data
+
+    def Space(self):
+        return self.Capacity - self.Count
+
+    def Clear(self):
+        self.Queue.Clear()
+        self._MetaReset()
+        self._MetaUpdate()
+
+    def _MetaReset(self):
+        self.ReadOffset = 0
+        self.WriteOffset = 0
+        self.Count = 0
+
+    def _MetaUpdate(self):
+        self.Queue.WriteMeta(self.Capacity, self.Count,
+                             self.ReadOffset, self.WriteOffset)
+
+    def _OffsetAdvance(self, offset):
+        offset = offset + 1
+        if offset >= self.Capacity:
+            offset = 0
+        return offset
+
+    def _CountInc(self):
+        self.Count = self.Count + 1
+
+    def _CountDec(self):
+        self.Count = self.Count - 1
---
+++ test/test_NvQueue.py
@@ -0,0 +1,147 @@
+import sys
+sys.path.append('../src/')
+
+# Test libraries
+import unittest
+from TestUtil import TestUtil
+
+# Unit Under Test
+from middleware.NvQueue import NvQueue
+
+# Other
+import uos as os
+
+
+class test_NvQueue(unittest.TestCase):
+
+    DATA_FMT = "<HH"
+    FILE = "./tmp"
+    CAPACITY = 4
+    Queue = None
+
+    def setUp(arg):
+        test_NvQueue.Queue = NvQueue.NvQueue(test_NvQueue.FILE, test_NvQueue.DATA_FMT, test_NvQueue.CAPACITY)
+        return
+
+    def tearDown(arg):
+        os.remove(test_NvQueue.FILE)
+        return
+
+    def test_Constructor(self):
+        file_exists = TestUtil.FileExists(test_NvQueue.FILE)
+        self.assertTrue(file_exists)
+        self.assertEqual(self.Queue.Capacity, test_NvQueue.CAPACITY)
+        self.assertEqual(self.Queue.Count, 0)
+        self.assertEqual(self.Queue.ReadOffset, 0)
+        self.assertEqual(self.Queue.WriteOffset, 0)
+
+    def test_PushOne(self):
+        w_data = (1, 2)
+
+        count = self.Queue.Push(*w_data)
+
+        self.assertEqual(count, 1)
+        self.assertNotEqual(self.Queue.WriteOffset, 0)
+
+    def test_PushMaxPlusOne(self):
+        w_data = (1, 2)
+
+        for i in range(0, self.Queue.Capacity):
+            count = self.Queue.Push(*w_data)
+            self.assertEqual(count, i + 1)
+
+        self.assertEqual(self.Queue.Push(*w_data), -1)
+
+    def test_PopOne(self):
+        w_data = (1, 2)
+
+        self.Queue.Push(*w_data)
+        r_data = self.Queue.Pop()
+
+        self.assertEqual(r_data, w_data)
+        self.assertEqual(self.Queue.Count, 0)
+        self.assertEqual(self.Queue.ReadOffset, self.Queue.WriteOffset)
+
+    def test_Space(self):
+        w_data = (1, 2)
+
+        self.Queue.Push(*w_data)
+        self.Queue.Push(*w_data)
+
+        self.assertEqual(self.Queue.Space(), self.Queue.Capacity - 2)
+
+        self.Queue.Pop()
+
+        self.assertEqual(self.Queue.Space(), self.Queue.Capacity - 1)
+
+    def test_PopMaxPlusOne(self):
+
+        for i in range(0, self.Queue.Capacity):
+            w_data = (i, i + 1)
+            print(w_data)
+            count = self.Queue.Push(*w_data)
+            self.assertEqual(count, i + 1)
+
+        for i in range(0, self.Queue.Capacity):
+            r_data = self.Queue.Pop()
+            print(r_data)
+            w_data = (i, i + 1)
+            self.assertEqual(r_data, w_data)
+            self.assertEqual(self.Queue.Count, self.Queue.Capacity - (i + 1))
+
+        self.assertEqual(self.Queue.Pop(), None)
+
+    def test_Clear(self):
+        w_data = (1, 2)
+
+        self.Queue.Push(*w_data)
+        self.Queue.Clear()
+        self.assertEqual(self.Queue.Count, 0)
+
+        queue = NvQueue.NvQueue(test_NvQueue.FILE, test_NvQueue.DATA_FMT, test_NvQueue.CAPACITY)
+        self.assertEqual(queue.Count, 0)
+        self.assertEqual(queue.ReadOffset, 0)
+        self.assertEqual(queue.WriteOffset, 0)
+
+    def test_ReloadExistingAtConstruction(self):
+        w_data = (1, 2)
+        self.Queue.Push(*w_data)
+        self.Queue.Push(*w_data)
+        self.Queue.Pop()
+        w_offset = self.Queue.WriteOffset
+        r_offset = self.Queue.ReadOffset
+
+        queue = NvQueue.NvQueue(test_NvQueue.FILE, test_NvQueue.DATA_FMT, test_NvQueue.CAPACITY)
+
+        self.assertEqual(queue.Count, 1)
+        self.assertEqual(queue.WriteOffset, w_offset)
+        self.assertEqual(queue.ReadOffset, r_offset)
+
+    def test_ReloadExistingAtConstructionCapacityChanged(self):
+        w_data = (1, 2)
+        self.Queue.Push(*w_data)
+        w_offset = self.Queue.WriteOffset
+
+        queue = NvQueue.NvQueue(test_NvQueue.FILE, test_NvQueue.DATA_FMT, test_NvQueue.CAPACITY + 1)
+
+        self.assertEqual(queue.Count, 1)
+        self.assertEqual(queue.WriteOffset, w_offset)
+        self.assertEqual(queue.Capacity, test_NvQueue.CAPACITY + 1)
+
+    def test_Iterator(self):
+
+        for i in range(0, self.Queue.Capacity - 1):
+            w_data = (i, i + 1)
+            print(w_data)
+            count = self.Queue.Push(*w_data)
+            self.assertEqual(count, i + 1)
+
+        i = 0
+        for item in self.Queue:
+            w_data = (i, i + 1)
+            self.assertEqual(item, w_data)
+            print(item)
+            i = i + 1
+
+        self.assertEqual(i, self.Queue.Capacity - 1)
+
