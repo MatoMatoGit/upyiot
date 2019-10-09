fa6c93c74d837663bfa46b72c47e136108e506a3
Dorus <spijker.dorus@gmail.com>
2019-10-09 20:27:04 +0200

    * src/module/DataExchange/ExchangeBuffer: Renamed to MessageBuffer.

---
+++ src/module/DataExchange/MessageBuffer.py
@@ -0,0 +1,48 @@
+import uerrno
+import ustruct
+from middleware.NvQueue import NvQueue
+from Message import Message
+
+class MessageBuffer:
+
+    MSG_STRUCT_TYPE     = const(0)
+    MSG_STRUCT_SUBTYPE  = const(1)
+    MSG_STRUCT_DATA     = const(2)
+
+    MsgDataLen = 30
+    MsgStructFmt = "<ii"
+    Directory = ""
+
+    _UnPackBuffer = None
+
+    @staticmethod
+    def Configure(directory, msg_len_max):
+        MessageBuffer.MsgDataLen = msg_len_max
+        MessageBuffer.MsgStructFmt = MessageBuffer.MsgStructFmt + \
+                                     str(MessageBuffer.MsgDataLen) + "s"
+        MessageBuffer.Directory = directory
+        MessageBuffer._UnPackBuffer = bytearray(msg_len_max * 1.5)
+
+    def __init__(self, file_prefix, msg_type, msg_subtype, max_entries):
+        file_path = MessageBuffer.Directory + file_prefix + str(msg_type) \
+                    + "_" + str(msg_subtype)
+        self.Queue = NvQueue.NvQueue(file_path, MessageBuffer.MsgStructFmt,
+                                      max_entries)
+        self.MsgType = msg_type
+        self.MsgSubtype = msg_subtype
+
+    def MessagePut(self, msg_string):
+        if len(msg_string) > MessageBuffer.MsgDataLen:
+            return -1
+        # TODO: Use this instead when NvQueue / StructFile can utilize an external buffer.
+        #ustruct.pack_into(MessageBuffer.MsgStructFmt, MessageBuffer._UnPackBuffer, 0,
+        #                  self.MsgType, self.MsgSubtype, msg_string)
+        return self.Queue.Push(self.MsgType, self.MsgSubtype, msg_string)
+
+    def MessageGet(self, msg_buffer=None):
+        # TODO: Use the provided message buffer to unpack the struct.
+        # TODO: This must be implemented in NvQueue / StructFile.
+        return self.Queue.Pop()
+
+    def MessageCount(self):
+        return self.Queue.Count
