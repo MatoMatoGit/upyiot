b2eb235c20abf59e9f6ab6b889310bde818858f7
Dorus <spijker.dorus@gmail.com>
2019-10-18 18:10:46 +0200

    + test/test_DataExchange: Tests test_RegisterMessageTypeDirectionBoth and test_MessagePut.

--- test/test_DataExchange.py
+++ test/test_DataExchange.py
@@ -66,6 +66,44 @@ class test_DataExchange(unittest.TestCase):
         self.assertIsNotNone(msg_map[DataExchange.MSG_MAP_RECV_BUFFER])
         self.assertIsInstance(msg_map[DataExchange.MSG_MAP_RECV_BUFFER], MessageBuffer)
 
+    def test_RegisterMessageTypeDirectionBoth(self):
+        msg_type = 3
+        msg_subtype = 3
+        msg_url = "/sensor/temp"
+        msg_dir = DataExchange.MSG_DIRECTION_BOTH
+
+        self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
+        msg_map = self.DataEx.MessageMapFromType(msg_type, msg_subtype)
+
+        self.assertEqual(msg_map[DataExchange.MSG_MAP_TYPE], msg_type)
+        self.assertEqual(msg_map[DataExchange.MSG_MAP_SUBTYPE], msg_subtype)
+        self.assertEqual(msg_map[DataExchange.MSG_MAP_URL], msg_url)
+        self.assertIsNotNone(msg_map[DataExchange.MSG_MAP_RECV_BUFFER])
+        self.assertIsInstance(msg_map[DataExchange.MSG_MAP_RECV_BUFFER], MessageBuffer)
+
+
+    def test_MessagePut(self):
+        msg_type = 3
+        msg_subtype = 3
+        msg_url = "/sensor/temp"
+        msg_dir = DataExchange.MSG_DIRECTION_BOTH
+
+        msg = {"test": "msg"}
+        self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
+        res = self.DataEx.MessagePut(msg, msg_type, msg_subtype)
+        msg_tuple = self.DataEx.SendMessageBuffer.MessageGet()
+
+        self.assertEqual(res, 1)
+        self.assertEqual(msg_tuple[MessageBuffer.MSG_STRUCT_TYPE], -1)
+        self.assertEqual(msg_tuple[MessageBuffer.MSG_STRUCT_SUBTYPE], -1)
+        self.assertEqual(Message.Deserialize(msg_tuple[MessageBuffer.MSG_STRUCT_DATA])[Message.MSG_SECTION_DATA], msg)
+
+    def test_MessageGet(self):
+        return
+
+
+
+
 
 class test_Endpoint(unittest.TestCase):
 
