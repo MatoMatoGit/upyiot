e7d333e2ba0a2b79e60d6d2a646a7a29ea04f177
Dorus <spijker.dorus@gmail.com>
2019-10-20 13:33:43 +0200

    + test/test_DataExchange: test_ServicePublishMessage, test_ServicePublishReceiveMessageRoundTrip. + test/test_DataExchange: Endpoint tests.

--- test/test_DataExchange.py
+++ test/test_DataExchange.py
@@ -14,7 +14,6 @@ from module.DataExchange.DataExchange import Endpoint
 from module.DataExchange.Message import Message
 from module.DataExchange.MessageBuffer import MessageBuffer
 
-
 class test_DataExchange(unittest.TestCase):
 
     DIR = "./"
@@ -23,19 +22,32 @@ class test_DataExchange(unittest.TestCase):
 
     MqttClient = None
     DataEx = None
+    RecvTopic = None
+    RecvMsg = None
+    RecvMsgCount = 0
 
     def setUp(arg):
+        test_DataExchange.RecvMsgCount = 0
+        test_DataExchange.RecvTopic = None
+        test_DataExchange.RecvMsg = None
         test_DataExchange.MqttClient = MQTTClient()
         test_DataExchange.DataEx = DataExchange(test_DataExchange.DIR, test_DataExchange.MqttClient,
                                                 test_DataExchange.ID, test_DataExchange.RETRIES)
 
     def tearDown(arg):
+        test_DataExchange.DataEx.Reset()
         return
 
     def test_Constructor(self):
         self.assertIsNotNone(self.DataEx.SendMessageBuffer)
         self.assertEqual(Message.DeviceId(), test_DataExchange.ID)
         self.assertTrue(self.DataEx.SendMessageBuffer.IsConfigured())
+
+    @staticmethod
+    def MqttMsgRecvCallback(topic, msg):
+        test_DataExchange.RecvTopic = topic
+        test_DataExchange.RecvMsg = msg
+        test_DataExchange.RecvMsgCount = test_DataExchange.RecvMsgCount + 1
 
     def test_RegisterMessageTypeDirectionSend(self):
         msg_type = 1
@@ -134,8 +146,8 @@ class test_DataExchange(unittest.TestCase):
         msg_tuple = self.DataEx.SendMessageBuffer.MessageGet()
 
         self.assertEqual(res, 1)
-        self.assertEqual(msg_tuple[MessageBuffer.MSG_STRUCT_TYPE], -1)
-        self.assertEqual(msg_tuple[MessageBuffer.MSG_STRUCT_SUBTYPE], -1)
+        self.assertEqual(msg_tuple[MessageBuffer.MSG_STRUCT_TYPE], msg_type)
+        self.assertEqual(msg_tuple[MessageBuffer.MSG_STRUCT_SUBTYPE], msg_subtype)
         self.assertEqual(Message.Deserialize(
             msg_tuple[MessageBuffer.MSG_STRUCT_DATA])[Message.MSG_SECTION_DATA], msg)
 
@@ -154,7 +166,7 @@ class test_DataExchange(unittest.TestCase):
         buf.MessagePut(Message.Stream().getvalue().decode('utf-8'))
         recv_msg = self.DataEx.MessageGet(msg_type, msg_subtype)
         print(recv_msg)
-
+        print(type(recv_msg))
         self.assertEqual(recv_msg[Message.MSG_SECTION_DATA], msg)
 
     def test_MessageGetNoMessageReceived(self):
@@ -165,7 +177,6 @@ class test_DataExchange(unittest.TestCase):
 
         self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
         recv_msg = self.DataEx.MessageGet(msg_type, msg_subtype)
-        print(recv_msg)
 
         self.assertEqual(recv_msg, -3)
 
@@ -230,10 +241,52 @@ class test_DataExchange(unittest.TestCase):
 
         self.assertTrue(subscription)
 
+    def test_ServicePublishMessage(self):
+        msg_type = 3
+        msg_subtype = 3
+        msg_url = "/sensor/temp"
+        msg = {"test": "msg"}
+        msg_dir = DataExchange.MSG_DIRECTION_SEND
 
+        # Initialize the Service on the first run
+        self.DataEx.Service()
 
+        # Override the MQTT message receive callback set by the Service
+        # so messages are received by the test.
+        self.MqttClient.set_callback(test_DataExchange.MqttMsgRecvCallback)
+        # Subscribe to the topic.
+        self.MqttClient.subscribe(msg_url)
 
+        self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
+        self.DataEx.MessagePut(msg, msg_type, msg_subtype)
 
+        # Run the Service again to publish the message
+        self.DataEx.Service()
+
+        recv_msg = Message.Deserialize(self.RecvMsg)
+        self.assertEqual(self.RecvMsgCount, 1)
+        print(recv_msg)
+        self.assertEqual(recv_msg[Message.MSG_SECTION_DATA], msg)
+
+    def test_ServicePublishAndReceiveRoundTrip(self):
+        msg_type = 3
+        msg_subtype = 3
+        msg_url = "/sensor/temp"
+        msg = {"test": "msg"}
+        msg_dir = DataExchange.MSG_DIRECTION_BOTH
+
+        # Initialize the Service on the first run
+        self.DataEx.Service()
+
+        self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
+        self.DataEx.MessagePut(msg, msg_type, msg_subtype)
+
+        # Run the Service again to publish and check for received messages.
+        self.DataEx.Service()
+
+        recv_msg = self.DataEx.MessageGet(msg_type, msg_subtype)
+        print(recv_msg)
+        self.assertEqual(recv_msg[Message.MSG_SECTION_DATA], msg)
 
 
 class test_Endpoint(unittest.TestCase):
@@ -244,16 +297,50 @@ class test_Endpoint(unittest.TestCase):
 
     MqttClient = None
     DataEx = None
+    Ep = None
 
     def setUp(arg):
-        test_DataExchange.MqttClient = MQTTClient()
-        test_DataExchange.DataEx = DataExchange(test_DataExchange.DIR, test_DataExchange.MqttClient,
-                                                test_DataExchange.ID, test_DataExchange.RETRIES)
+        test_Endpoint.MqttClient = MQTTClient()
+        test_Endpoint.DataEx = DataExchange(test_Endpoint.DIR, test_Endpoint.MqttClient,
+                                            test_Endpoint.ID, test_Endpoint.RETRIES)
+        test_Endpoint.Ep = Endpoint()
 
     def tearDown(arg):
+        test_Endpoint.DataEx.Reset()
         return
 
     def test_Constructor(self):
+        self.assertEqual(self.Ep.DataEx, self.DataEx)
 
-        return
+    def test_MultipleInstances(self):
+        ep = Endpoint()
+        self.assertEqual(self.Ep.DataEx, self.DataEx)
+        self.assertEqual(ep.DataEx, self.DataEx)
 
+    def test_MessagePut(self):
+        msg_type = 3
+        msg_subtype = 3
+        msg_url = "/sensor/temp"
+        msg = {"test": "msg"}
+        msg_dir = DataExchange.MSG_DIRECTION_BOTH
+
+        self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
+        res = self.Ep.MessagePut(msg, msg_type, msg_subtype)
+
+        self.assertEqual(res, 1)
+
+    def test_MessageGet(self):
+        msg_type = 3
+        msg_subtype = 3
+        msg_url = "/sensor/temp"
+        msg = {"test": "msg"}
+        msg_dir = DataExchange.MSG_DIRECTION_BOTH
+
+        self.DataEx.RegisterMessageType(msg_type, msg_subtype, msg_url, msg_dir)
+        msg_map = self.DataEx.MessageMapFromType(msg_type, msg_subtype)
+        buf = msg_map[DataExchange.MSG_MAP_RECV_BUFFER]
+
+        Message.Serialize(123, msg, msg_type, msg_subtype)
+        buf.MessagePut(Message.Stream().getvalue().decode('utf-8'))
+        recv_msg = self.Ep.MessageGet(msg_type, msg_subtype)
+        self.assertEqual(recv_msg[Message.MSG_SECTION_DATA], msg)
