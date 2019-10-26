817832bfcd78ac6d8a00efc44cd30aab8f76a4df
Dorus <spijker.dorus@gmail.com>
2019-10-26 20:40:57 +0200

    + src/module/Messaging/MessageSpecification: Method CreateFromFile. Parse JSON and create a class instance.

--- src/module/Messaging/MessageSpecification.py
+++ src/module/Messaging/MessageSpecification.py
@@ -1,3 +1,11 @@
+import ujson
+
+MSG_SPEC_KEY_TYPE       = "type"
+MSG_SPEC_KEY_SUBTYPE    = "stype"
+MSG_SPEC_KEY_DATA       = "data"
+MSG_SPEC_KEY_URL        = "url"
+MSG_SPEC_KEY_DIR        = "dir"
+
 
 class MessageSpecification:
 
@@ -15,3 +23,16 @@ class MessageSpecification:
                                                                    self.DataDef,
                                                                    self.Url,
                                                                    self.Direction)
+
+def CreateFromFile(file):
+
+    f_in = open(file, 'r')
+    json = f_in.read()
+    f_in.close()
+
+    spec = ujson.loads(json)
+    return MessageSpecification(spec[MSG_SPEC_KEY_TYPE],
+                                spec[MSG_SPEC_KEY_SUBTYPE],
+                                spec[MSG_SPEC_KEY_DATA],
+                                spec[MSG_SPEC_KEY_URL],
+                                spec[MSG_SPEC_KEY_DIR])
