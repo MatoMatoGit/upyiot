f8f084be084a428f82cb84832eb3f4e78c7eedd5
Dorus <spijker.dorus@gmail.com>
2019-10-27 09:09:43 +0100

    + test/test_ExtLogging: UTs for Extended Logging.

---
+++ test/test_ExtLogging.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append('../src/')
+
+# Test libraries
+import unittest
+from TestUtil import TestUtil
+
+# Unit Under Test
+from middleware.ExtLogging import ExtLogging
+from middleware.ExtLogging.ExtLogging import ExtLogger
+
+# Other
+
+class test_ExtLogging(unittest.TestCase):
+
+
+    def setUp(arg):
+        return
+
+    def tearDown(arg):
+        return
+
+    def test_ConfigGlobal(self):
+
+        ExtLogging.ConfigGlobal(level=logging.INFO, file="./log")
+
+        log = ExtLogger("test")
+
+        log.info("hi")
+
+ConfigGlobal(level=DEBUG, file="./log")
+
+log = ExtLogger("test")
+
+log.info("hi")
+
+log.debug("hoi")
