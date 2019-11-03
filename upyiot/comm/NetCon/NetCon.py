e991e06466b5bf1d373f1dcbf554cf81dfcbfe23
Dorus <spijker.dorus@gmail.com>
2019-11-03 11:18:52 +0100

    + upyiot/comm/NetCon/NetCon: Use Service class for NetConService. * upyiot/comm/NetCon/NetCon: Fixed indentation of entire file.

--- upyiot/comm/NetCon/NetCon.py
+++ upyiot/comm/NetCon/NetCon.py
@@ -1,113 +1,140 @@
-import os
-import network
+import uos as os
 import utime
-import Log
+from micropython import const
+from module.Service.Service import Service
+from module.Service.Service import ServiceException
 
-class NetCon:
-	FILE_NET_CON = "net_con"
-	PATH_NET_CON = "/" + FILE_NET_CON
-	CONN_RETRIES = 5
-	
-	def _init_(self, net_con_dir, ap_cfg, log_en):
-		self.RootDir = net_con_dir
-		self.Connected = False
-		self.ApSsid = ap_cfg[0]
-		self.ApPwd = ap_cfg[1]
-		self.ApIp = ap_cfg[2]
-		self.Log = Log.Log("NetCon", log_en)
-	
-	def AccessPointStart(self):
-		self.Log.Print("Starting AP mode. SSID: {}, IP: {}".format(self.ApSsid, self.ApIp))
-		self.NetIf = network.WLAN(network.AP_IF)
-		self.NetIf.ifconfig((self.ApIp, '255.255.255.0', '192.168.0.1', '192.168.0.1'))
-		self.NetIf.active(True)
-		self.NetIf.config(essid=self.ApSsid, password=self.ApPwd)
-		
-		
-	def AccessPointStop(self):
-		self.Log.Print("Stopping AP mode.")
-		self.NetIf.disconnect()
-		self.NetIf.active(False)
 
-	def StationSettingsStore(self, ssid, pwd):
-		self.Log.Print("Storing station settings.")
-		self._NetConFileWrite(ssid, pwd)
-	
-	def StationSettingsReset(self):
-		self.Log.Print("Resetting station settings.")
-		self._NetConFileDelete()
-		
-	def StationSettingsAreSet(self):
-		return self._NetConFileExists()
-	
-	def StationStart(self):
-		if self._NetConFileExists() is False:
-			self.Log.Print("No station settings stored.")
-			return False
-		elif self.Connected is True:
-			self.Log.Print("Already connected.")
-			return False
-		else:
-			ssid, pwd = self._NetConFileRead()
-			self.Log.Print("Starting station mode. Connecting to SSID: {}".format(ssid))
-			self.NetIf = network.WLAN(network.STA_IF)
-			self.NetIf.active(True)
-			
-			# Attempt to connect to the configured AP, if
-			# the attempt fails retry.
-			i = 0
-			while i < self.CONN_RETRIES:
-				self.NetIf.connect(ssid, pwd)
-				utime.sleep(5)				
-				self.Connected = self.NetIf.isconnected()
-				if self.Connected is True:
-					self.Log.Print("Connected. ifconfig: {}".format(self.NetIf.ifconfig()))
-					break
-				else:
-					self.Log.Print("Failed to connect.")
-					i = i + 1
-					self.Log.Print("Connect retries remaining: {}".format((self.CONN_RETRIES - i)))
+class NetConExceptionNoStationSettings(ServiceException):
 
-			return self.Connected
-	
-	def StationStop(self):
-		self.NetIf.disconnect()
-		self.NetIf.active(False)
-		self.Log.Print("Stopping station mode.")
-		if self.Connected is True:
-			self.Log.Print("Disconnected.")
-			self.Connected = False
-		
-		
+    def __init__(self):
+        super().__init__()
 
-	def _NetConFileExists(self):
-		ls_dir = os.listdir(self.RootDir)
-		self.Log.Print("Directory {} contains {}.".format(self.RootDir, ls_dir), "FileExists")
-		for node in ls_dir:
-			self.Log.Print("File {}".format(node), "FileExists")
-			if self.FILE_NET_CON in node:
-				return True;
 
-		return False;
-	
-	def _NetConFileWrite(self, ssid, pwd):
-		f = open(self.RootDir + self.PATH_NET_CON, 'w')
-		f.write(ssid + '\n')
-		f.write(pwd + '\n')
-		f.close()
-	
-	def _NetConFileRead(self):
-		f = open(self.RootDir + self.PATH_NET_CON, 'r')
-		ssid = f.readline().strip('\n')
-		pwd = f.readline().strip('\n')
-		f.close()
-		self.Log.Print("SSID: {}, PWD: {}".format(ssid, pwd), "FileRead")
-		return ssid, pwd
-	
-	def _NetConFileDelete(self):
-		self.Log.Print("Deleting file {}".format(self.RootDir + self.PATH_NET_CON))
-		try:
-			os.remove(self.RootDir + self.PATH_NET_CON)
-		except:
-			pass
+class NetConService(Service):
+    NET_CON_SERVICE_MODE = Service.MODE_RUN_ONCE
+
+    def __init__(self):
+        super().__init__(self.NET_CON_SERVICE_MODE, ())
+
+
+class NetCon(NetConService):
+
+    FILE_NET_CON = "net_con"
+    PATH_NET_CON = "/" + FILE_NET_CON
+    CONN_RETRIES = 5
+
+    MODE_STATION        = const(0)
+    MODE_ACCESS_POINT   = const(1)
+
+    def __init__(self, net_con_dir, ap_cfg, mode, wlan_if_obj):
+        super().__init__()
+        self.RootDir = net_con_dir
+        self.ApSsid = ap_cfg["ssid"]
+        self.ApPwd = ap_cfg["pwd"]
+        self.ApIp = ap_cfg["ip"]
+        self.NetIf = wlan_if_obj
+        self.WlanMode = mode
+
+    def SvcRun(self):
+        if self.WlanMode is NetCon.MODE_ACCESS_POINT:
+            self.AccessPointStart()
+        else:
+            self.StationStart()
+
+    def SvcDeinit(self):
+        if self.WlanMode is NetCon.MODE_ACCESS_POINT:
+            self.AccessPointStop()
+        else:
+            self.StationStop()
+
+    def AccessPointStart(self):
+        print("[NetCon] Starting AP mode. SSID: {}, IP: {}".format(self.ApSsid, self.ApIp))
+        self.NetIf.ifconfig((self.ApIp, '255.255.255.0', '192.168.0.1', '192.168.0.1'))
+        self.NetIf.active(True)
+        self.NetIf.config(essid=self.ApSsid, password=self.ApPwd)
+
+    def AccessPointStop(self):
+        print("[NetCon] Stopping AP mode.")
+        self.NetIf.disconnect()
+        self.NetIf.active(False)
+
+    def StationSettingsStore(self, ssid, pwd):
+        print("[NetCon] Storing station settings.")
+        self._NetConFileWrite(ssid, pwd)
+
+    def StationSettingsLoad(self):
+        return self._NetConFileRead()
+
+    def StationSettingsReset(self):
+        print("[NetCon] Resetting station settings.")
+        self._NetConFileDelete()
+
+    def StationSettingsAreSet(self):
+        return self._NetConFileExists()
+
+    def StationStart(self):
+        if self._NetConFileExists() is False:
+            print("[NetCon] No station settings stored.")
+            raise NetConExceptionNoStationSettings
+        elif self.NetIf.isconnected() is True:
+            print("[NetCon] Already connected.")
+            self.SvcSuspend()
+        else:
+            ssid, pwd = self._NetConFileRead()
+            print("[NetCon] Starting station mode. Connecting to SSID: {}".format(ssid))
+            self.NetIf.active(True)
+
+            # Attempt to connect to the configured AP, if
+            # the attempt fails retry.
+            i = 0
+            while i < self.CONN_RETRIES:
+                self.NetIf.connect(ssid, pwd)
+                utime.sleep(5)
+                if self.NetIf.isconnected() is True:
+                    print("[NetCon] Connected. ifconfig: {}".format(self.NetIf.ifconfig()))
+                    break
+                else:
+                    print("[NetCon] Failed to connect.")
+                    i = i + 1
+                    print("[NetCon] Connect retries remaining: {}".format((self.CONN_RETRIES - i)))
+
+    def StationStop(self):
+        self.NetIf.disconnect()
+        self.NetIf.active(False)
+        print("[NetCon] Stopping station mode.")
+
+    def IsConnected(self):
+        return self.NetIf.isconnected()
+
+    def _NetConFileExists(self):
+        f_exists = True
+        try:
+            f = open(self.RootDir + self.PATH_NET_CON)
+            f.close()
+        except OSError:
+            f_exists = False
+
+        return f_exists
+
+    def _NetConFileWrite(self, ssid, pwd):
+        f = open(self.RootDir + self.PATH_NET_CON, 'w')
+        f.write(ssid + '\n')
+        f.write(pwd + '\n')
+        f.close()
+
+    def _NetConFileRead(self):
+        f = open(self.RootDir + self.PATH_NET_CON, 'r')
+        ssid = f.readline().strip('\n')
+        pwd = f.readline().strip('\n')
+        f.close()
+        print("[NetCon] SSID: {}, PWD: {}".format(ssid, pwd))
+        return ssid, pwd
+
+    def _NetConFileDelete(self):
+        print("[NetCon] Deleting file {}".format(self.RootDir + self.PATH_NET_CON))
+        try:
+            os.remove(self.RootDir + self.PATH_NET_CON)
+        except OSError:
+            pass
 
