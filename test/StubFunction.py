2b28e9314aef12e587435b91e0fcf87aa03b3693
Dorus <spijker.dorus@gmail.com>
2019-11-03 11:21:04 +0100

    + test/StubFunction: Option to set a call handler and a specific exception to raise. * test/StubFunction: Changed some names.

--- test/StubFunction.py
+++ test/StubFunction.py
@@ -2,19 +2,29 @@
 
 class StubFunction:
 
-    def __init__(self):
+    def __init__(self, func=None, exc=None):
         self.RaiseExc = False
         self.CallCount = 0
+        self.Func = func
         self.Result = None
+        self.Exc = exc
 
-    def SetRaiseExc(self):
+    def RaiseExcSet(self):
         self.RaiseExc = True
 
-    def SetResult(self, result):
+    def ResultSet(self, result):
         self.Result = result
 
     def __call__(self):
         self.CallCount += 1
+        print("[StubFunc] Call count: {}".format(self.CallCount))
+        if self.Func is not None:
+            print("[StubFunc] Calling function: {}".format(self.Func))
+            self.Func(self)
         if self.RaiseExc is True:
-            raise Exception
+            print("[StubFunc] Raising exception")
+            if self.Exc is None:
+                raise Exception
+            else:
+                raise self.Exc
         return self.Result
