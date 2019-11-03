7e5f38d55a4d39b78c07f0fb673714d28597d8e7
Dorus <spijker.dorus@gmail.com>
2019-11-03 11:18:00 +0100

    + upyiot/module/Service/ServiceScheduler: SchedulerExceptions. * upyiot/module/Service/ServiceScheduler: Split Run to several private methods, complete scheduling functionality.

--- upyiot/module/Service/ServiceScheduler.py
+++ upyiot/module/Service/ServiceScheduler.py
@@ -1,23 +1,44 @@
 from module.Service.Service import Service
+from module.Service.Service import ServiceExceptionSuspend
+from module.Service.Service import ServiceException
 import utime
+from micropython import const
+
+
+class SchedulerException(Exception):
+
+    def __init__(self):
+        return
+
+
+class SchedulerExceptionStopped(SchedulerException):
+
+    def __init__(self):
+        return
 
 
 class ServiceScheduler:
 
-    def __init__(self):
+    SCHEDULER_RESULT_OK = const(0)
+    SCHEDULER_RESULT_ERR = const(-1)
+
+    def __init__(self, sleep_time_sec=1):
         self.Services = list()
-        self.Ticks = 0
+        self.RunTimeSec = 0
+        self.Cycles = None
+        self.Index = 0
+        self.SleepTimeSec = sleep_time_sec
         return
 
     def ServiceRegister(self, service):
         res = 0
         try:
-            if service.IsInitialized() is False:
-                service.Init()
-                service.StateSet(Service.STATE_SUSPENDED)
+            if service.SvcIsInitialized() is False:
+                service.SvcInit()
+                service.SvcStateSet(Service.STATE_SUSPENDED)
                 self.Services.append(service)
         except:
-            service.StateSet(Service.STATE_DISABLED)
+            service.SvcStateSet(Service.STATE_DISABLED)
             res = -1
 
         return res
@@ -25,81 +46,120 @@ class ServiceScheduler:
     def ServiceDeregister(self, service):
         res = 0
         try:
-            if service.IsInitialized() is True:
-                service.Deinit()
-                service.StateSet(Service.STATE_UNINITIALIZED)
+            if service.SvcIsInitialized() is True:
+                service.SvcDeinit()
+                service.SvcStateSet(Service.STATE_UNINITIALIZED)
         except:
-            service.StateSet(Service.STATE_DISABLED)
+            service.SvcStateSet(Service.STATE_DISABLED)
             res = -1
         finally:
             self.Services.remove(service)
         return res
 
     def Run(self, cycles=None):
+        self.Cycles = cycles
 
-        # Infinite loop
-        while True:
-            self.Ticks += 1
-
-            # Activate periodic services.
-            for svc in self.Services:
-                if svc.Mode is Service.MODE_RUN_PERIODIC:
-                    # If interval time has passed, activate the service.
-                    if self.Ticks + svc.LastRun >= svc.Interval:
-                        svc.Activate()
-
-            # Service run loop.
-            # Runs a service until none are ready.
-            index = 0
-            svc_ran = False
+        try:
+            # Infinite loop
             while True:
-                if index >= len(self.Services):
-                    break
+                self._UpdatePeriodicServices()
 
-                service = self.Services[index]
-
-                # Check if all services that this service is dependent on
-                # ran at least once. If this is the case the service is ready
-                # to run.
-                if service.IsActive() and self._CheckServiceDependencies(service) is True:
-                    service.StateSet(Service.STATE_READY)
-
-                # If the service is ready to run, call its Run method.
-                if service.IsReady() is True:
-                    svc_ran = True
-                    service.Deactivate()
-                    try:
-                        service.Run()
-                        service.StateSet(Service.STATE_SUSPENDED)
-                        # Update the last run timestamp.
-                        service.LastRun(self.Ticks)
-                    except:
-                        service.StateSet(Service.STATE_DISABLED)
-
-                # Increment the service list index.
-                # Wrap around if the end if reached.
-                index += 1
-                if index >= len(self.Services):
-                    index = 0
-                    # A minimum of one service must
-                    # have ran.
-                    if svc_ran is False:
-                        break
+                # Service run loop.
+                # Runs a service until none are ready.
+                self.Index = 0
+                while True:
                     svc_ran = False
 
-            # No services are ready at this moment.
-            utime.sleep(1)
+                    if self.Index >= len(self.Services):
+                        print("[Scheduler] No services registered")
+                        break
 
-            if cycles is not None:
-                cycles -= 1
-                if cycles is 0:
-                    break
+                    service = self.Services[self.Index]
+
+                    if service.SvcStateGet() is not service.STATE_DISABLED:
+                        print("[Scheduler] Selected service: {}".format(service))
+                        # Check if all services that this service is dependent on
+                        # ran at least once. If this is the case the service is ready
+                        # to run.
+                        if service.SvcIsActive() and self._CheckServiceDependencies(service) is True:
+                            service.SvcStateSet(Service.STATE_READY)
+
+                        # If the service is ready to run, call its Run method.
+                        if service.SvcIsReady() is True:
+                            print("[Scheduler] Running service")
+                            svc_ran = True
+                            self._ServiceRun(service)
+                    else:
+                        print("[Scheduler] Service {} is disabled".format(service))
+
+                    print("[Scheduler] Selecting next service")
+                    if self._IndexAdvance(svc_ran) is False:
+                        print("[Scheduler] No more services to run")
+                        break
+                    print("[Scheduler] Next index: {}".format(self.Index))
+
+                self.RunTimeSec += 1
+                print("[Scheduler] Sleeping for {} seconds".format(self.SleepTimeSec))
+                # No services are ready at this moment.
+                utime.sleep(self.SleepTimeSec)
+
+                self._CyclesDec()
+
+        except SchedulerExceptionStopped:
+            print("[Scheduler] Stopped.")
+
+    def _ServiceRun(self, service):
+        try:
+            service.SvcRun()
+            service.SvcStateSet(Service.STATE_SUSPENDED)
+        except ServiceExceptionSuspend:
+            print("[Scheduler] Suspending service")
+            service.SvcStateSet(Service.STATE_SUSPENDED)
+        except ServiceException:
+            print("[Scheduler] Error occurred, disabling service.")
+            service.SvcStateSet(Service.STATE_DISABLED)
+
+        # Update the last run timestamp.
+        service.SvcLastRunSet(self.RunTimeSec)
+        service.SvcDeactivate()
+
+    def _UpdatePeriodicServices(self):
+        # Activate periodic services.
+        for svc in self.Services:
+            if svc.SvcMode is Service.MODE_RUN_PERIODIC:
+                print("[Scheduler] Updating periodic service: {}".format(svc))
+                # If interval time has passed, activate the service.
+                if self.RunTimeSec + svc.SvcLastRun >= svc.SvcInterval:
+                    print("[Scheduler] Activating periodic service: {}".format(svc))
+                    svc.SvcActivate()
 
     def _CheckServiceDependencies(self, service):
         ready = True
-        for svc_dep in service.ServiceDeps:
-            if svc_dep.LastRun() is 0:
-                svc_dep.Schedule()
+        for svc_dep in service.SvcDeps:
+            print("[Scheduler] Checking dependency: {}".format(svc_dep))
+            if svc_dep.SvcLastRun is 0:
+                svc_dep.SvcActivate()
                 ready = False
-
+        print("[Scheduler] Dependencies ran: {}".format(ready))
         return ready
+
+    def _IndexAdvance(self, svc_ran):
+        print("[Scheduler] Service ran: {}".format(svc_ran))
+        # Increment the service list index.
+        # Wrap around if the end if reached.
+        self.Index += 1
+
+        if self.Index >= len(self.Services):
+            self.Index = 0
+            # A minimum of one service must
+            # have ran.
+            return svc_ran
+
+        return True
+
+    def _CyclesDec(self):
+        if self.Cycles is not None:
+            self.Cycles -= 1
+            print("[Scheduler] Cycles left: {}.".format(self.Cycles))
+            if self.Cycles is 0:
+                raise SchedulerExceptionStopped
