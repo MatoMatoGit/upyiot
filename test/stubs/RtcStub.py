812772c5e20f12a2326576cced49e4ff5953fc09
Dorus <spijker.dorus@gmail.com>
2019-11-09 01:06:30 +0100

    - test/stubs/RtcStub: now method.

--- test/stubs/RtcStub.py
+++ test/stubs/RtcStub.py
@@ -4,11 +4,11 @@ class RTC(object):
     DateTime = 0
     
     def __init__(self):
+        self.DateTime = 0
         return 
-    
-    def now(self):
-        return self.DateTime
-    
+
     def datetime(self, date_time):
+        if date_time is None:
+            return self.DateTime
         self.DateTime = date_time
         print(self.DateTime)
