8848adcbeb11a5c962c971774508619949ac56ac
Dorus <spijker.dorus@gmail.com>
2019-12-09 01:03:52 +0100

    * upyiot/drivers/Sensors/CapMoisture: Changed pin mode to IN. Removed hard=True argument from irq, caused issues.

--- upyiot/drivers/Sensors/CapMoisture.py
+++ upyiot/drivers/Sensors/CapMoisture.py
@@ -8,20 +8,19 @@ class CapMoisture(SensorBase):
     PulseCount = 0
 
     def __init__(self, pulse_pin_nr, pwr_pin_nr):
-        self.FreqCounterPin = Pin(pulse_pin_nr, mode=Pin.OUT)
-        self.FreqCounterPin.irq(trigger=Pin.IRQ_FALLING, handler=self._IrqHandlerFreqCounterPin, hard=True)
+        self.FreqCounterPin = Pin(pulse_pin_nr, mode=Pin.IN)
+        self.FreqCounterPin.irq(trigger=Pin.IRQ_FALLING, handler=CapMoisture._IrqHandlerFreqCounterPin)
         self.PowerPin = Pin(pwr_pin_nr, Pin.OUT)
         return
 
     def Read(self):
-        self.PulseCount = 0
+        CapMoisture.PulseCount = 0
         self.PowerPin.on()
         time.sleep(1)
         self.PowerPin.off()
-        return PulseCount
+        return CapMoisture.PulseCount
 
     @staticmethod
     def _IrqHandlerFreqCounterPin(pin_obj):
-        global PulseCount
+        CapMoisture.PulseCount += 1
 
-        PulseCount += 1
