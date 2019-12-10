44f32aaea550d1605a98e75543039c8b37e7711f
Dorus <spijker.dorus@gmail.com>
2019-12-10 22:55:05 +0100

    * upyiot/drivers/Led/Led: Invert logic in Toggle method.

--- upyiot/drivers/Led/Led.py
+++ upyiot/drivers/Led/Led.py
@@ -25,7 +25,7 @@ class Led(object):
         return self.PWM_DUTY_MIN
     
     def On(self):
-        if (self.Pwm == True):
+        if self.Pwm is True:
             self.Pin.duty(self.PWM_DUTY_MAX)
         else:
             self.Pin.on()
@@ -33,7 +33,7 @@ class Led(object):
         self.IsOn = True
     
     def Off(self):
-        if (self.Pwm == True):
+        if self.Pwm is True:
             self.Pin.duty(0)
         else:
             self.Pin.off()
@@ -41,15 +41,15 @@ class Led(object):
         self.IsOn = False
         
     def Toggle(self):
-        if (self.IsOn == True):
+        if self.IsOn is False:
             self.On()
-            self.IsOn = False
+            self.IsOn = True
         else:
             self.Off()
-            self.IsOn = True
+            self.IsOn = False
             
     def State(self):
-        if (self.IsOn == True):
+        if self.IsOn is True:
             return 1
         else:
             return 0
@@ -57,13 +57,13 @@ class Led(object):
     # Get or set the LED intensity.
     def Intensity(self, value=None):
         # Check if the LED is configured in PWM mode.
-        if (self.Pwm == True):
+        if self.Pwm is True:
             # If a value argument is passed set the duty 
             # cycle.
             if value is not None:
-                if (value > self.PWM_DUTY_MAX):
+                if value > self.PWM_DUTY_MAX:
                     value = self.PWM_DUTY_MAX
-                elif (value < self.PWM_DUTY_MIN):
+                elif value < self.PWM_DUTY_MIN:
                     value = self.PWM_DUTY_MIN
                 self.Pin.duty(value)
             # If no value is passed get the duty cycle.
