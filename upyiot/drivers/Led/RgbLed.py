d0e02ac90011d8043b1619809a6881ad9ab36593
Dorus <spijker.dorus@gmail.com>
2019-12-25 17:47:09 +0100

    + upyiot/drivers/Led/RgbLed: On and Off methods. ColorSet method. Color definitions. * upyiot/drivers/Led/RgbLed: Renamed method Colors -> ColorsSet.

--- upyiot/drivers/Led/RgbLed.py
+++ upyiot/drivers/Led/RgbLed.py
@@ -1,13 +1,37 @@
-import Led
+from upyiot.driver.Led.Led import Led
+from micropython import const
 
-class RgbLed(object):
-    
+
+class RgbLed:
+
+    RGB_COLOR_RED   = const(0)
+    RGB_COLOR_GREEN = const(1)
+    RGB_COLOR_BLUE  = const(2)
+
     def __init__(self, pin_red, pin_green, pin_blue):
         self.Red = Led(pin_red, True)
         self.Green = Led(pin_green, True)
         self.Blue = Led(pin_blue, True)
-        
-    def Color(self, red, green, blue):
+
+    def ColorSet(self, color, value):
+        if color is self.RGB_COLOR_RED:
+            self.Red.Intensity(value)
+        elif color is self.RGB_COLOR_GREEN:
+            self.Green.Intensity(value)
+        elif color is self.RGB_COLOR_BLUE:
+            self.Blue.Intensity(value)
+
+    def ColorsSet(self, red, green, blue):
         self.Red.Intensity(red)
         self.Green.Intensity(green)
         self.Blue.Intensity(blue)
+
+    def On(self):
+        self.Red.On()
+        self.Green.On()
+        self.Blue.On()
+
+    def Off(self):
+        self.Red.Off()
+        self.Green.Off()
+        self.Blue.Off()
