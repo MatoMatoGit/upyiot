b95d0d6b200240496093290aaf1809e55b1f21c1
Dorus <spijker.dorus@gmail.com>
2020-02-02 16:41:03 +0100

    + upyiot/drivers/Sensors/Mcp9700Temp|/PhTLight: Enable pin.

--- upyiot/drivers/Sensors/Mcp9700Temp.py
+++ upyiot/drivers/Sensors/Mcp9700Temp.py
@@ -4,10 +4,14 @@ from machine import Pin, ADC
 
 class Mcp9700Temp(SensorBase):
 
-    def __init__(self, temp_pin_nr):
+    def __init__(self, temp_pin_nr, temp_en_pin_nr):
         self.TempAdc = ADC(Pin(temp_pin_nr))
+        self.TempEn = Pin(temp_en_pin_nr, Pin.OUT)
         self.TempAdc.atten(ADC.ATTN_11DB)
         self.TempAdc.width(ADC.WIDTH_10BIT)
 
     def Read(self):
-        return self.TempAdc.read()
+        self.TempEn.on()
+        val = self.TempAdc.read()
+        self.TempEn.off()
+        return val
--- upyiot/drivers/Sensors/PhTLight.py
+++ upyiot/drivers/Sensors/PhTLight.py
@@ -4,10 +4,15 @@ from machine import Pin, ADC
 
 class PhTLight(SensorBase):
 
-    def __init__(self, pht_pin_nr):
+    def __init__(self, pht_pin_nr, pht_pin_en_nr):
         self.PhTAdc = ADC(Pin(pht_pin_nr))
+        self.PhTEn = Pin(pht_pin_en_nr, Pin.OUT)
         self.PhTAdc.atten(ADC.ATTN_11DB)
         self.PhTAdc.width(ADC.WIDTH_10BIT)
 
     def Read(self):
-        return self.PhTAdc.read()
+        self.PhTEn.on()
+        val = self.PhTAdc.read()
+        self.PhTEn.off()
+        return val
+
