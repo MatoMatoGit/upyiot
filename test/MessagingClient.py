7e18c131449fbc7e515d93f30f294a9045551069
Dorus <spijker.dorus@gmail.com>
2020-02-10 15:14:15 +0100

    + test/MessagingClient: Pass message list as argument. Create MessageSpecifications for each message type from spec. + test/MessagingClient: Use id from the topic when storing messages.

--- test/MessagingClient.py
+++ test/MessagingClient.py
@@ -14,25 +14,38 @@ try:
 except:
     import os
 
+import ujson as json
+
+
 MessageSpecs = set()
-OUTPUT_DIR = "./"
 MsgCount = 0
+_MsgListFile = None
+_MsgList = {}
+_BaseDir = "./"
 
 
 def ComposePath(base, type, subtype):
     return base + "/" + str(type) + "." + str(subtype)
 
 
+def SplitTopic(topic):
+    return topic.split('/')
+
+
 def MqttMsgRecvCallback(topic, message):
     global MsgCount
+    global _BaseDir
 
     topic = topic.decode('utf-8')
+    topic_lvls = SplitTopic(topic)
 
     print("[MsgClient] Received message {} on topic {}".format(message, topic))
     for spec in MessageSpecs:
-        if spec.Url == topic:
-            # TODO: Replace ID in file path with actual ID, extract from topic.
-            file_path = ComposePath(OUTPUT_DIR, spec.Type, spec.Subtype) + "/a4cf127d8b2c_" + str(MsgCount) + ".json"
+        spec_topic_lvls = SplitTopic(spec.Url)
+        print(spec_topic_lvls)
+        if spec_topic_lvls[2] == topic_lvls[2]:
+            file_path = ComposePath(_BaseDir, spec.Type, spec.Subtype) + "/" + \
+                        topic_lvls[1] + "_" + str(MsgCount) + ".json"
             print("[MsgClient] Writing message to file {}".format(file_path))
             f = open(file_path, "w")
             f.write(message)
@@ -42,33 +55,53 @@ def MqttMsgRecvCallback(topic, message):
     print("[MsgClient] Topic {} not supported.".format(topic))
 
 
-def main(msg_spec_file):
+def main(argv):
     ID = 'server_node'
     BROKER = '192.168.0.103'
     PORT = 1883
 
-    # TODO: Loop through files in a folder. Create message specs from each one.
-    msg_spec = MessageSpecification.CreateFromFile(msg_spec_file)
-    print(msg_spec)
-    MessageSpecs.add(msg_spec)
+    global _MsgListFile
+    global _MsgList
+    global _BaseDir
+
+    _MsgListFile = argv[0]
+    print(_MsgListFile)
+
+    _BaseDir = argv[1]
+    print(_BaseDir)
+
+    msg_file = open(_MsgListFile, 'r')
+    msg_file_str = msg_file.read()
+    print(msg_file_str)
+    msg_file.close()
+
+    _MsgList = json.loads(msg_file_str)
+
+    for msg_entry in _MsgList.keys():
+        print(msg_entry)
+        msg_spec = MessageSpecification.CreateFromFile(_MsgList[msg_entry]["spec"])
+        print(msg_spec)
+        MessageSpecs.add(msg_spec)
+
+        output_dir = ComposePath(_BaseDir, msg_spec.Type, msg_spec.Subtype)
+        print("Creating folder: {}".format(output_dir))
+        try:
+            os.mkdir(output_dir)
+        except OSError:
+            print("Could not create folder")
 
     mqtt_client = MQTTClient(ID, BROKER, PORT)
     mqtt_client.set_callback(MqttMsgRecvCallback)
     mqtt_client.connect()
 
     for spec in MessageSpecs:
+        print("Subscribing to: {}".format(spec.Url))
         mqtt_client.subscribe(spec.Url)
 
-        output_dir = ComposePath(OUTPUT_DIR, spec.Type, spec.Subtype)
-        try:
-            os.mkdir(output_dir)
-        except OSError:
-            print("Could not create folder")
-
     while True:
-        mqtt_client.check_msg()
-        time.sleep(5)
+        while mqtt_client.check_msg() is None:
+            time.sleep(3)
 
 
 if __name__ == '__main__':
-    main("./SensorReportMoist.json")
+    main(sys.argv[1:])
