d82c1e96f834709eee4db5d8762f6a5ed0c3b082
Dorus <spijker.dorus@gmail.com>
2020-03-21 16:17:45 +0100

    + upyiot/drivers/Sensors/Mcp9700Temp: Raw to Celcius conversion.

--- upyiot/drivers/Sensors/Mcp9700Temp.py
+++ upyiot/drivers/Sensors/Mcp9700Temp.py
@@ -5,6 +5,11 @@ from micropython import const
 
 class Mcp9700Temp(SensorBase):
 
+    V_SUPPLY = 3.3
+    RANGE_MAX = 1024
+    OFFSET = 0.4
+    DIV = 0.01
+
     def __init__(self, temp_pin_nr, en_supply_obj):
         self.TempAdc = ADC(Pin(temp_pin_nr))
         self.TempEn = en_supply_obj
@@ -14,6 +19,7 @@ class Mcp9700Temp(SensorBase):
     def Read(self):
         self.TempEn.Enable()
         val = self.TempAdc.read()
+        val = self._RawToCelsius(val)
         self.TempEn.Disable()
         return val
 
@@ -22,3 +28,10 @@ class Mcp9700Temp(SensorBase):
 
     def Disable(self):
         self.TempEn.Disable()
+
+    @staticmethod
+    def _RawToCelsius(raw_temp):
+        cel_temp = raw_temp * (Mcp9700Temp.V_SUPPLY / Mcp9700Temp.RANGE_MAX)
+        cel_temp -= Mcp9700Temp.OFFSET
+        cel_temp /= Mcp9700Temp.DIV
+        return cel_temp
