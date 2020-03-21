dc21e491380a5005726bc84f35701d859911465a
Dorus <spijker.dorus@gmail.com>
2020-03-21 16:18:35 +0100

    * upyiot/drivers/Board/Supply: Call Enable if default is enabled. + upyiot/drivers/Board/Supply: Logging.

--- upyiot/drivers/Board/Supply.py
+++ upyiot/drivers/Board/Supply.py
@@ -9,27 +9,34 @@ class Supply:
 
     def __init__(self, en_pin_nr, settle_time_ms, inverse_pol=False, default_state=SUPPLY_STATE_DISABLED):
         if default_state is self.SUPPLY_STATE_ENABLED:
-            self.EnCount = 1
+            self.Enable()
         else:
             self.EnCount = 0
+            self.EnPin.off()
 
         self.EnPin = Signal(en_pin_nr, mode=Pin.OUT, invert=inverse_pol)
         self.SettleTime = settle_time_ms
 
     def Enable(self):
-        if self.EnCount is 0:
+        self.EnCount += 1
+        print("[Supply] Enable count: {}".format(self.EnCount))
+
+        if self.EnCount is 1:
+            print("[Supply] Enabling supply")
             self.EnPin.on()
             utime.sleep_ms(self.SettleTime)
-
-        self.EnCount += 1
         return 0
 
     def Disable(self):
         if self.EnCount is 0:
+            print("[Supply] Supply cannot be disabled")
             return -1
 
         self.EnCount -= 1
+        print("[Supply] Enable count: {}".format(self.EnCount))
+
         if self.EnCount is 0:
+            print("[Supply] Disabling supply")
             self.EnPin.off()
             utime.sleep_ms(self.SettleTime)
 
