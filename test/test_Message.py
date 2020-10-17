fdf44ade8ef7c7037b2ecf2622f3c6e4dc46d87d
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2020-10-17 13:01:59 +0200

    + test/test_Message: Use of MessageTemplate and metadata functions. More assertions on round-trip results. + test/test_Message: Test for parsing multiple messages.

--- test/test_Message.py
+++ test/test_Message.py
@@ -8,12 +8,41 @@ from TestUtil import TestUtil
 
 # Unit Under Test
 from upyiot.comm.Messaging.Message import Message
+from upyiot.comm.Messaging.MessageTemplate import *
+from upyiot.comm.Messaging.Parser.JsonParser import JsonParser
+from upyiot.system.SystemTime.SystemTime import SystemTime
 
 # Other
 import uio
+import utime
+
+def DeviceId():
+    return "123"
 
 class test_Message(unittest.TestCase):
 
+    # JSON representation
+    MSG_SECTION_META = "meta"
+    MSG_SECTION_DATA = "data"
+    MSG_META_DATETIME = "dt"
+    MSG_META_VERSION = "ver"
+    MSG_META_TYPE = "type"
+    MSG_META_SUBTYPE = "stype"
+    MSG_META_ID = "id"
+
+    TimeInstance = SystemTime.InstanceGet()
+
+    Metadata = {
+        MSG_META_DATETIME:  123,
+        MSG_META_VERSION:   1,
+        MSG_META_TYPE:      1,
+        MSG_META_SUBTYPE:   1,
+        MSG_META_ID:        DeviceId(),
+    }
+
+    MetadataFuncs = {
+        MSG_META_DATETIME: TimeInstance.Epoch,
+    }
 
     def setUp(arg):
         return
@@ -21,49 +50,107 @@ class test_Message(unittest.TestCase):
     def tearDown(arg):
         return
 
-    def test_DeviceIdSet(self):
-        id_set = 0x200
-
-        Message.DeviceId(id_set)
-
-        self.assertEqual(Message.Msg[Message.MSG_SECTION_META][Message.MSG_META_ID], id_set)
-
-    def test_DeviceIdGet(self):
-        id_set = 0x200
-
-        Message.DeviceId(id_set)
-        id_get = Message.DeviceId()
-
-        self.assertEqual(id_set, id_get)
-        Message.Print()
-
     def test_SerializeDeserializeRoundTrip(self):
 
         msg_key = "msg"
         msg_test = "test"
-        id = 0x200
-        datetime = '2019-10-01T23:00:00'
+        dev_id = DeviceId()
+        datetime = int(utime.time())
         data = {
             msg_key: msg_test,
         }
-        type = 1
+        msg_type = 1
         sub_type = 2
 
-        Message.DeviceId(id)
+        MessageTemplate.SectionsSet(self.MSG_SECTION_META, self.MSG_SECTION_DATA)
+        MessageTemplate.MetadataTemplateSet(test_Message.Metadata, test_Message.MetadataFuncs)
 
-        buffer = Message.Serialize(datetime, data, type, sub_type)
+        parser = JsonParser()
+        Message.SetParser(parser)
+
+        meta = {
+            self.MSG_META_TYPE:      msg_type,
+            self.MSG_META_SUBTYPE:   sub_type,
+        }
+        buffer = Message.Serialize(data, meta)
         msg_str = buffer.getvalue().decode('utf-8')
 
-        print(msg_str)
+        print("Serialized message: {}".format(msg_str))
 
         Message.Deserialize(str(msg_str))
 
         Message.Print()
-        self.assertEqual(Message.Msg[Message.MSG_SECTION_META][Message.MSG_META_DATETIME], datetime)
-        self.assertEqual(Message.Msg[Message.MSG_SECTION_META][Message.MSG_META_TYPE], type)
-        self.assertEqual(Message.Msg[Message.MSG_SECTION_META][Message.MSG_META_SUBTYPE], sub_type)
-        self.assertEqual(Message.Msg[Message.MSG_SECTION_META][Message.MSG_META_ID], id)
-        self.assertEqual(Message.Msg[Message.MSG_SECTION_DATA][msg_key], msg_test)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_DATETIME], datetime)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_TYPE], msg_type)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_SUBTYPE], sub_type)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_ID], dev_id)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_DATA][msg_key], msg_test)
 
+    def test_SerializeDeserializeNoMetadataArgument(self):
 
+        msg_key = "msg"
+        msg_test = "test"
+        dev_id = DeviceId()
+        datetime = int(utime.time())
+        data = {
+            msg_key: msg_test,
+        }
 
+        MessageTemplate.SectionsSet(self.MSG_SECTION_META, self.MSG_SECTION_DATA)
+        MessageTemplate.MetadataTemplateSet(test_Message.Metadata, test_Message.MetadataFuncs)
+
+        parser = JsonParser()
+        Message.SetParser(parser)
+
+        buffer = Message.Serialize(data)
+        msg_str = buffer.getvalue().decode('utf-8')
+
+        print("Serialized message: {}".format(msg_str))
+
+        Message.Deserialize(str(msg_str))
+
+        Message.Print()
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_DATETIME], datetime)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_TYPE], self.Metadata[self.MSG_META_TYPE])
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_SUBTYPE], self.Metadata[self.MSG_META_SUBTYPE])
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_ID], dev_id)
+        self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_DATA][msg_key], msg_test)
+
+    def test_SerializeDeserializeMultiple(self):
+        MessageTemplate.SectionsSet(self.MSG_SECTION_META, self.MSG_SECTION_DATA)
+        MessageTemplate.MetadataTemplateSet(test_Message.Metadata, test_Message.MetadataFuncs)
+
+        parser = JsonParser()
+        Message.SetParser(parser)
+
+        for i in range(0, 10):
+
+            msg_key = "msg"
+            msg_test = "test"
+
+            utime.sleep(1)
+
+            dev_id = DeviceId()
+            datetime = int(utime.time())
+            data = {
+                msg_key: msg_test,
+            }
+            msg_type = i
+            sub_type = i + 1
+
+            meta = {
+                self.MSG_META_TYPE:      msg_type,
+                self.MSG_META_SUBTYPE:   sub_type,
+            }
+            buffer = Message.Serialize(data, meta)
+            msg_str = buffer.getvalue().decode('utf-8')
+
+            print("Serialized message: {}".format(msg_str))
+
+            Message.Deserialize(str(msg_str))
+
+            self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_DATETIME], datetime)
+            self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_TYPE], msg_type)
+            self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_SUBTYPE], sub_type)
+            self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_META][self.MSG_META_ID], dev_id)
+            self.assertEqual(Message.Msg[MessageTemplate.MSG_SECTION_DATA][msg_key], msg_test)
