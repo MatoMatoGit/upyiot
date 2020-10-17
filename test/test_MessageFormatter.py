79b8d1bf36bf6ec867c5b4ead6f834edfd7e6841
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2020-10-17 12:50:53 +0200

    * test/test_MessageFormatAdapter: Renamed to test_MessageFormatter. + test/test_MessageFormatter: More test cases convering SEND_ON_X option.

---
+++ test/test_MessageFormatter.py
@@ -0,0 +1,160 @@
+import sys
+sys.path.append('../')
+
+# Test libraries
+import unittest
+from TestUtil import TestUtil
+
+# Unit Under Test
+from upyiot.comm.Messaging.MessageFormatter import MessageFormatter
+
+# Other
+from ExampleMessage import ExampleMessage
+
+
+class MsgExStub:
+
+    def __init__(self):
+        self.Data = None
+        self.Meta = None
+        self.Type = 0
+        self.SubType = 0
+        return
+
+    def GetLastMessage(self):
+        return self.Data
+
+    def MessagePut(self, msg_data_dict, msg_type, msg_subtype, msg_meta_dict=None):
+        self.Data = msg_data_dict
+        self.Type = msg_type
+        self.SubType = msg_subtype
+        self.Meta = msg_meta_dict
+        print("[MsgExStub] Put message of type {}.{}: {} ".format(msg_type, msg_subtype, msg_data_dict))
+
+    def MessageGet(self, msg_type, msg_subtype):
+        print("[MsgExStub] Getting message")
+
+
+class test_MessageFormatter(unittest.TestCase):
+
+    def setUp(arg):
+        return
+
+    def tearDown(arg):
+        return
+
+    def test_SendOnChange(self):
+        msg_spec = ExampleMessage()
+
+        msg_ex = MsgExStub()
+        fmt = MessageFormatter(msg_ex, MessageFormatter.SEND_ON_CHANGE, msg_spec)
+
+        observer = fmt.CreateObserver(ExampleMessage.DATA_KEY_N)
+        stream = fmt.CreateStream(ExampleMessage.DATA_KEY_ARRAY)
+
+        inputs = fmt.GetInputs()
+        print("Formatter inputs: {}".format(inputs))
+
+        observer.Update(3)
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 3)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [])
+
+        stream.write([1, 2, 3])
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 3)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2, 3])
+
+        observer.Update(4)
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 4)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2 , 3])
+
+        stream.write([1, 2, 3, 4])
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 4)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2, 3, 4])
+
+        return
+
+    def test_SendOnComplete(self):
+        msg_spec = ExampleMessage()
+
+        msg_ex = MsgExStub()
+        fmt = MessageFormatter(msg_ex, MessageFormatter.SEND_ON_COMPLETE, msg_spec)
+
+        observer = fmt.CreateObserver(ExampleMessage.DATA_KEY_N)
+        stream = fmt.CreateStream(ExampleMessage.DATA_KEY_ARRAY)
+
+        inputs = fmt.GetInputs()
+        print("Formatter inputs: {}".format(inputs))
+
+        observer.Update(3)
+
+        data = msg_ex.GetLastMessage()
+        self.assertEqual(data, None)
+
+        stream.write([1, 2, 3])
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 3)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2, 3])
+
+        observer.Update(4)
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 4)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2, 3])
+
+        stream.write([1, 2, 3, 4])
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 4)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2, 3, 4])
+
+        return
+
+    def test_SendOnCompleteAppend(self):
+        msg_spec = ExampleMessage()
+
+        msg_ex = MsgExStub()
+        fmt = MessageFormatter(msg_ex, MessageFormatter.SEND_ON_COMPLETE, msg_spec)
+
+        observer = fmt.CreateObserver(ExampleMessage.DATA_KEY_N)
+        stream = fmt.CreateStream(ExampleMessage.DATA_KEY_ARRAY, 3)
+
+        inputs = fmt.GetInputs()
+        print("Formatter inputs: {}".format(inputs))
+
+        observer.Update(3)
+        stream.write(1)
+        stream.write(2)
+        stream.write(3)
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 3)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [1, 2, 3])
+
+        observer.Update(4)
+        stream.write(4)
+        stream.write(5)
+        stream.write(6)
+
+        data = msg_ex.GetLastMessage()
+        print("Data from MsgExStub: {}".format(data))
+        self.assertEqual(data[ExampleMessage.DATA_KEY_N], 4)
+        self.assertEqual(data[ExampleMessage.DATA_KEY_ARRAY], [4, 5, 6])
+
+        return
