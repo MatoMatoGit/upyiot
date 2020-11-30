7b34ed919ec94dd0f3629c262a580d1aa4149d4a
Dorus <spijker.dorus@gmail.com>
2020-11-30 01:06:21 +0100

    + upyiot/system/Util/DeviceId: Hexlify and decode device ID.

--- upyiot/system/Util/DeviceId.py
+++ upyiot/system/Util/DeviceId.py
@@ -1,16 +1,26 @@
 import machine
+import ubinascii
 
 _DeviceId = None
 
+
 def DeviceIdSet(device_id):
+    global _DeviceId
     _DeviceId = device_id
 
+
 def DeviceIdString():
+    global _DeviceId
+
     if _DeviceId is None:
         return str(ubinascii.hexlify(machine.unique_id()).decode('utf-8'))
     return str(_DeviceId)
 
+
 def DeviceId():
+    global _DeviceId
+
     if _DeviceId is None:
-        return machine.unique_id()
+        return ubinascii.hexlify(machine.unique_id()).decode('utf-8')
     return _DeviceId
+
