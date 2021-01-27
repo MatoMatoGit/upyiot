b678e547b144074a3ab22fb096513b364a378487
Dorus <spijker.dorus@gmail.com>
2021-01-27 20:13:02 +0100

    + upyiot/comm/Messaging/MessageFormatter: MessagePartSink concept WIP.

--- upyiot/comm/Messaging/MessageFormatter.py
+++ upyiot/comm/Messaging/MessageFormatter.py
@@ -13,7 +13,7 @@ class MessagePartSource:
         self.Count = 0
         self.CompleteCount = complete_count
 
-    def MessagePart(self, data):
+    def Put(self, data):
         if self.Count is 0:
             self.MsgFormatter.MessagePartAdd(self.Key, data)
         else:
@@ -25,6 +25,28 @@ class MessagePartSource:
         if self.Count is self.CompleteCount:
             self.Count = 0
             self.MsgFormatter.MessagePartFinalize()
+            
+
+class MessagePartSink:
+
+    def __init__(self, msg_formatter_obj, key, complete_count):
+        self.MsgFormatter = msg_formatter_obj
+        self.Key = key
+        self.Count = complete_count
+        self.CompleteCount = complete_count
+
+    def Get(self, data):
+        if self.Count is 0:
+            self.MsgFormatter.MessagePartAdd(self.Key, data)
+        else:
+            self.MsgFormatter.MessagePartAppend(self.Key, data)
+        self._CountDec()
+
+    def _CountDec(self):
+        self.Count -= 1
+        if self.Count <= 0:
+            self.Count = self.CompleteCount
+            self.MsgFormatter.MessagePartFinalize()
 
 
 class MessagePartObserver(MessagePartSource, Observer):
@@ -33,7 +55,7 @@ class MessagePartObserver(MessagePartSource, Observer):
         super().__init__(msg_formatter_obj, key, complete_count)
 
     def Update(self, arg):
-        self.MessagePart(arg)
+        self.Put(arg)
 
 
 class MessagePartStream(MessagePartSource):
@@ -42,16 +64,16 @@ class MessagePartStream(MessagePartSource):
         super().__init__(msg_formatter_obj, key, complete_count)
 
     def write(self, data):
-        self.MessagePart(data)
-
+        self.Put(data)
+        
 
 class MessageFormatter:
     SEND_ON_CHANGE      = const(0)
     SEND_ON_COMPLETE    = const(1)
 
     def __init__(self, msg_ex_obj, mode, msg_spec_obj, msg_meta_dict=None):
-        self.MsgDef = msg_spec_obj.DataDef.copy()
-        Log.info("Definition: {}".format(self.MsgDef))
+        self.MsgOut = msg_spec_obj.DataDef.copy()
+        Log.info("Definition: {}".format(self.MsgOut))
         self.Mode = mode
         self.Inputs = set()
         self.MsgType = msg_spec_obj.Type
@@ -64,14 +86,14 @@ class MessageFormatter:
         return self.Inputs
 
     def CreateObserver(self, key, count=1):
-        if key not in self.MsgDef:
+        if key not in self.MsgOut:
             return -1
         observer = MessagePartObserver(self, key, count)
         self.Inputs.add(observer)
         return observer
 
     def CreateStream(self, key, count=1):
-        if key not in self.MsgDef:
+        if key not in self.MsgOut:
             return -1
         stream = MessagePartStream(self, key, count)
         self.Inputs.add(stream)
@@ -79,23 +101,23 @@ class MessageFormatter:
 
     def MessagePartAdd(self, key, value):
         Log.debug("Adding part: {}:{}".format(key, value))
-        if type(self.MsgDef[key]) is list:
+        if type(self.MsgOut[key]) is list:
             if type(value) is list:
-                self.MsgDef[key] = value
+                self.MsgOut[key] = value
             else:
-                self.MsgDef[key].clear()
-                self.MsgDef[key].append(value)
+                self.MsgOut[key].clear()
+                self.MsgOut[key].append(value)
         else:
-            self.MsgDef[key] = value
+            self.MsgOut[key] = value
 
     def MessagePartAppend(self, key, value):
         Log.debug("Appending part: {}:{}".format(key, value))
         # If the message value is a string, append it.
         if type(value) is str:
-            self.MsgDef[key] += value
+            self.MsgOut[key] += value
         else:
             # Append the new value to the list.
-            self.MsgDef[key].append(value)
+            self.MsgOut[key].append(value)
 
     def MessagePartFinalize(self):
         self.PartCount += 1
@@ -105,10 +127,10 @@ class MessageFormatter:
         # If all parts of the message have been updated, or
         # the message must be sent on any change,
         # hand over the message to the Messaging Exchange class.
-        if self.PartCount is len(self.MsgDef) or \
+        if self.PartCount is len(self.MsgOut) or \
                 self.Mode is MessageFormatter.SEND_ON_CHANGE:
-            Log.info("Handover to MsgEx: {}".format(self.MsgDef))
-            res = self.MsgEx.MessagePut(self.MsgDef, self.MsgType,
+            Log.info("Handover to MsgEx: {}".format(self.MsgOut))
+            res = self.MsgEx.MessagePut(self.MsgOut, self.MsgType,
                                            self.MsgSubtype, self.MsgMeta)
 
             if res is -1:
