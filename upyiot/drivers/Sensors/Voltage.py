63bc8a8c596c457cc0c5504b29a10d2c14a067df
Dorus <spijker.dorus@gmail.com>
2021-05-08 18:46:55 +0200

    + upyiot/drivers/Sensors/Voltage: Optional max voltage to calculate attentuation.

--- upyiot/drivers/Sensors/Voltage.py
+++ upyiot/drivers/Sensors/Voltage.py
@@ -7,25 +7,45 @@ class VoltageSensor(SensorBase):
 
     RANGE_MAX = const(1024)
 
-    def __init__(self, pin_nr, en_supply_obj):
+    VoltageToAttnMap = {
+        1.0: ADC.ATTN_0DB,
+        1.34: ADC.ATTN_2_5DB,
+        2.0: ADC.ATTN_6DB,
+        3.6: ADC.ATTN_11DB
+    }
+
+    def __init__(self, pin_nr: int, en_supply_obj=None, max_voltage: float = 3.3):
         self.VoltAdc = ADC(Pin(pin_nr))
         self.Supply = en_supply_obj
-        self.VoltAdc.atten(ADC.ATTN_11DB)
-        self.VoltAdc.width(ADC.WIDTH_10BIT)
+
+        self.VoltAdc.atten(self._VoltageToAttenuation(max_voltage))
+        self.VoltAdc.width(ADC.WIDTH_12BIT)
 
     def Read(self):
-        self.Supply.Enable()
         val = self.VoltAdc.read()
-        val = self._RawToVoltage(val)
-        self.Supply.Disable()
+        # val = self._RawToVoltage(val)
         return val
 
     def Enable(self):
-        self.Supply.Enable()
+        self._EnableSupply()
 
     def Disable(self):
-        self.Supply.Disable()
+        self._DisableSupply()
 
     def _RawToVoltage(self, raw):
-        volt = raw * (self.Supply.Voltage / self.RANGE_MAX)
+        volt = raw * (3.3 / self.RANGE_MAX)
         return volt
+
+    def _EnableSupply(self):
+        if self.Supply is not None:
+            self.Supply.Enable()
+
+    def _DisableSupply(self):
+        if self.Supply is not None:
+            self.Supply.Disable()
+
+    def _VoltageToAttenuation(self, voltage: float):
+        for v in self.VoltageToAttnMap.keys():
+            if voltage <= v:
+                return self.VoltageToAttnMap[v]
+        raise Exception("Voltage too high for ADC input. No attenuation level found.")
