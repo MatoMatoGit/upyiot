9c26fc6662d1e867ba6a0563bee20ee35ca5aba4
Dorus <spijker.dorus@gmail.com>
2021-07-03 11:39:46 +0200

    - upyiot/system/Util/Version: Disabled version storage.

--- upyiot/system/Util/Version.py
+++ upyiot/system/Util/Version.py
@@ -4,7 +4,7 @@ from upyiot.system.ExtLogging import ExtLogging
 
 class Version:
 
-    VERSION_DATA_FMT = "<III"
+    # VERSION_DATA_FMT = "<III"
 
     _Instance = None
 
@@ -17,30 +17,34 @@ class Version:
         """
         return Version._Instance
 
-    def __init__(self, dir, major, minor, patch):
+    def __init__(self, major, minor, patch):
         if Version._Instance is not None:
             raise Exception("An instance of the Version class already exists.")
 
         Version._Instance = self
-        self.Log = ExtLogging.Create("Version")
-        self.VersionSFile = StructFile.StructFile(dir + '/ver', self.VERSION_DATA_FMT)
+        self.Major = major
+        self.Minor = minor
+        self.Patch = patch
 
-        try:
-            self.Major, self.Minor, self.Patch = self.VersionSFile.ReadData(0)
-        except TypeError:
-            self.Log.info("No stored version present.")
-            self.Major = self.Minor = self.Patch = -1
+        # self.Log = ExtLogging.Create("Version")
+        # self.VersionSFile = StructFile.StructFile(dir + '/ver', self.VERSION_DATA_FMT)
 
-        self.Log.info("Current version (major.minor.patch): {}.{}.{}".format(self.Major, self.Minor, self.Patch))
+        # try:
+        #     self.Major, self.Minor, self.Patch = self.VersionSFile.ReadData(0)
+        # except TypeError:
+        #     self.Log.info("No stored version present.")
+        #     self.Major = self.Minor = self.Patch = -1
 
-        if major is not self.Major or minor is not self.Minor \
-                or patch is not self.Patch:
-            self.Major = major
-            self.Minor = minor
-            self.Patch = patch
-            self.VersionSFile.WriteData(0, self.Major, self.Minor, self.Patch)
+        # self.Log.info("Current version: v{}.{}.{}".format(self.Major, self.Minor, self.Patch))
 
-            self.Log.info("New version: {}.{}.{}".format(self.Major, self.Minor, self.Patch))
+        # if major is not self.Major or minor is not self.Minor \
+        #         or patch is not self.Patch:
+        #     self.Major = major
+        #     self.Minor = minor
+        #     self.Patch = patch
+        #     self.VersionSFile.WriteData(0, self.Major, self.Minor, self.Patch)
+
+        #     self.Log.info("New version: {}.{}.{}".format(self.Major, self.Minor, self.Patch))
 
     def SwVersionString(self):
         """
