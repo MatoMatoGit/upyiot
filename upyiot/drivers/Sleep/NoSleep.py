5b955fba617cd17aecb0559881fc5b4e8c95ecd6
Dorus <spijker.dorus@gmail.com>
2021-07-31 16:21:32 +0200

    * upyiot/drivers/Sleep/NoSleep: Do not use machine.reset

--- upyiot/drivers/Sleep/NoSleep.py
+++ upyiot/drivers/Sleep/NoSleep.py
@@ -1,6 +1,6 @@
-from upyiot.drivers.Sleep.DeepSleepBase import DeepSleepBase
+from upyiot.drivers.Sleep.DeepSleepBase import DeepSleepBase, DeepSleepExceptionFailed
 import utime
-import machine
+# import machine
 
 
 class NoSleep(DeepSleepBase):
@@ -8,7 +8,8 @@ class NoSleep(DeepSleepBase):
     def DeepSleep(self, msec):
         print("[NoSleep] Idle for {}".format(msec))
         utime.sleep(int(msec/1000))
-        machine.reset()
+        # machine.reset()
+        raise DeepSleepExceptionFailed
 
     def DeepSleepForever(self):
         while True:
